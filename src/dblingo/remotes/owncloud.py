9c03cb0e4c45fc34a55953685e2483fdb2a67810
Alexandar Mechev <apmechev@gmail.com>
2023-10-07 11:10:51 +0200

    fix: 🐛bypass Nextcloud remote if no link

--- src/dblingo/remotes/owncloud.py
+++ src/dblingo/remotes/owncloud.py
@@ -5,7 +5,12 @@ from . import AbstractRemote
 
 class OwncloudRemote(AbstractRemote):
     def __init__(self):
+        if not NEXTCLOUD_LINK or NEXTCLOUD_LINK == '' or NEXTCLOUD_LINK == 'your_link_here':
+            self.client = None
+            return
         self.client = owncloud.Client.from_public_link(NEXTCLOUD_LINK)
 
     def upload(self, file_path):
+        if not self.client:
+            return
         self.client.put_file(file_path, file_path)
